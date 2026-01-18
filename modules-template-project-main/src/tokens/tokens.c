49179fbbd484edbedb8590ba9757b39fd54e0502
PauCosSicres <pau.cos01@estudiant.upf.edu>
2026-01-18 22:58:48 +0100

    more avanced

--- modules-template-project-main/src/tokens/tokens.c
+++ modules-template-project-main/src/tokens/tokens.c
@@ -12,15 +12,21 @@
 #include "tokens.h"
 
 
-void tokens_init(void) {
-    fprintf(ofile, "[tokens] OK\n");
+void tokens_init(Tokenizer *tk, int line_num, char *full_line) {
+    tk->line_n = line_num;
+    tk->position = 0;
+    tk->full_line = full_line;
+}
+
+int tokenize(Tokenizer *tkz, Token *token_out){
+    // TODO   
 }
 
 // only used to get the wanted tokens
-char get_word(Token tok) {
-    char *word = malloc(tok.lenght + 1);
-    memcpy(word, tok.word, tok.lenght);
-    word[tok.lenght + 1] = "\0"; // string terminator
+char *get_word(Token tok) {
+    char *word = malloc(tok.length + 1);
+    memcpy(word, tok.word, tok.length);
+    word[tok.length] = '\0'; // string terminator
     return word;
 }
 
--- modules-template-project-main/src/tokens/tokens.h
+++ modules-template-project-main/src/tokens/tokens.h
@@ -9,6 +9,7 @@
  * -----------------------------------------------------------------------------
  */
 
+
 #ifndef TOKENS_H
 #define TOKENS_H
 
@@ -28,21 +29,37 @@ typedef enum {
 typedef struct {
     Token_type type;
     char *word;     // pointer to start of token (no malloc for all & faster)
-    int lenght;     // necessary to get full token word
-    int line;       // get this from PP_Core directly | here use it to make error line handling easier
+    int length;     // necessary to get full token word
+    int line_n;       // get this from PP_Core directly | here use it to make error line handling easier
 } Token;
 
 // tokenizer 
 typedef struct {
-    int line;       // current line from file (save here and pass it to all tokens)
+    int line_n;       // current line from file (save here and pass it to all tokens)
     int position;   // word position currently being tokenized
+    char *full_line;
 } Tokenizer;
 
 
+/*
+way to use it:
+
+Tokenizer tkz;
+tokens_init(&tkz, line_num, full_line) // both line_num and full_line are gotten from PP_core
+
+Token token_out;
+while( tokenize(&tkz, &token_out) ){
+    use individual token (depending on module does one thing or another)   
+ }
+
+*/
+
 // Functions that other modules should call
-void tokens_init(void);
+void tokens_init(Tokenizer *tk, int line_num, char *full_line);
 
-void tokenize_next();
+// 1 if word has been tokenized, 0 if end of line, so it works with while loop
+int tokenize(Tokenizer *tkz, Token *token_out); // line is raw line text gotten from PP_core
 
-char get_word(Token);
+char *get_word(Token);
 #endif // TOKENS_H
+
