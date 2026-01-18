3f81fbc328a8d5a746e74b44631d342c3be8605b
PauCosSicres <pau.cos01@estudiant.upf.edu>
2026-01-18 21:03:12 +0100

    tokenizer started

--- modules-template-project-main/src/tokens/tokens.h
+++ modules-template-project-main/src/tokens/tokens.h
@@ -16,6 +16,33 @@
 
 extern FILE* ofile;
 
+typedef enum {
+    IDENTIFIER,     // define, MAX, ifdef, x...
+    NUMBER,         // numbers 
+    SYMBOL,         // #, (), =, < ...
+    STRING,         // literals: "file.txt", "int", "for"....
+    END             // end of line "\n"
+} Token_type;
+
+// individual token structure
+typedef struct {
+    Token_type type;
+    char *word;     // pointer to start of token (no malloc for all & faster)
+    int lenght;     // necessary to get full token word
+    int line;       // get this from PP_Core directly | here use it to make error line handling easier
+} Token;
+
+// tokenizer 
+typedef struct {
+    int line;       // current line from file (save here and pass it to all tokens)
+    int position;   // word position currently being tokenized
+} Tokenizer;
+
+
+
 void tokens_init(void);
 
+void tokenize_next();
+
+char get_word(Token);
 #endif // TOKENS_H
