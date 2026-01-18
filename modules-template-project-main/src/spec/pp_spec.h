07cf61e1e7674203e80ed6f94c4e2888eff7b69e
Emmanuel Kwabena Cooper <167142060+u236098@users.noreply.github.com>
2026-01-18 19:36:53 +0000

    Add pp_spec.h header file with preprocessor definitions

---
+++ modules-template-project-main/src/spec/pp_spec.h
@@ -0,0 +1,11 @@
+#ifndef PP_SPEC_H
+#define PP_SPEC_H
+
+#define PP_MAX_IF_DEPTH 64
+
+#define PP_FLAG_C    "-c"
+#define PP_FLAG_D    "-d"
+#define PP_FLAG_ALL  "-all"
+#define PP_FLAG_HELP "-help"
+
+#endif
